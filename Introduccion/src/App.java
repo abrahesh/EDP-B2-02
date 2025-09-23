9001f2ae52b6cfe43dc5ff545b42e492e864e035
rarauw2 <raul.arroyo3@alu.uclm.es>
2025-09-23 12:51:42 +0200

    Implement class main with error

--- Introduccion/src/App.java
+++ Introduccion/src/App.java
@@ -1,5 +1,21 @@
+import java.io.FileNotFoundException;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        try {
+            FicheroSecuencial<Satelite> sf = new FicheroSecuencial<Satelite>("weather.csv",";"){
+                sf.skip();
+                while(!sf.isEndOfFile()){
+                    Satelite sat = new Satelite();
+                    sf.read(sat);
+                    if(sat.getMeanMotion() >= 10){
+                        System.out.println(sat);
+                    }
+                }
+                sf.close();
+            } catch (FileNotFoundException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
