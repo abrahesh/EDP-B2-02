3bf7031132f85eca2bb16ee0af41c26f83b3afde
abrah <abrahamescalona@live.com>
2025-09-23 12:20:47 +0200

    Implement FicheroSecuencial class for sequential file reading

--- Introduccion/src/FicheroSecuencial.java
+++ Introduccion/src/FicheroSecuencial.java
@@ -1,3 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class FicheroSecuencial {
+    private File file;
+    private Scanner scanner;
+
+    public FicheroSecuencial(String nombreFichero) throws FileNotFoundException{
+        file = new File(nombreFichero);
+        scanner = new Scanner(file);
+    }
+    
+    public String readLine(){
+        return scanner.nextLine();
+    }
+
+    public void skip(){
+        scanner.nextLine();
+    }
+    public void close(){
+        scanner.close();
+    }
+    public boolean isEOF(){
+        return !scanner.hasNextLine();
+    }
     
 }
