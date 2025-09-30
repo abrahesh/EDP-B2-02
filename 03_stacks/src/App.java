de6f168a585381f759aba29a02b5a7ae3587c312
abrah <abrahamescalona@live.com>
2025-09-30 12:31:45 +0200

    evaluar expresioens

---
+++ 03_stacks/src/App.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class App {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        Stack<String> pila;
+        String[] expr = leerExpresion();
+        pila = evaluarExpresion(expr);
+    }
+
+    private static Stack<String> invertir() {
+        Stack<String> pilaAuxiliar;
+
+    }
+
+    private static Stack<String> evaluarExpresion(String[] expr){
+        String cad, resultado, op1, op2;
+    }
+
+    private static String[] leerExpresion() {
+        System.out.println("Expresión a evaluar:");
+        String linea = scanner.nextLine();
+        StringTokenizer st = new StringTokenizer(linea);
+        String[] entrada = new String[st.countTokens()];
+        int i = 0;
+        while (st.hasMoreTokens()) {
+            entrada[i++] = st.nextToken();
+        }
+        return entrada;
+    }
+}
