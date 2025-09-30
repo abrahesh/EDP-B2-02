fe20f864e6ec5bdf4e524097c793e89420c05ac1
abrah <abrahamescalona@live.com>
2025-09-30 12:08:28 +0200

    Implement input reading functionality in practicapilas class

--- 03_stacks/src/practicapilas.java
+++ 03_stacks/src/practicapilas.java
@@ -4,5 +4,16 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class practicapilas {
-    
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private static String[] leerEntrada() {
+        String linea = scanner.nextLine();
+        StringTokenizer st = new StringTokenizer(linea);
+        String[] entrada = new String[st.countTokens()];
+        int i = 0;
+        while (st.hasMoreTokens()) {
+            entrada[i++] = st.nextToken();
+        }
+        return entrada;
+    }
 }
