2b0dbcfe299325965c354e4bffa612b3f82cc7ff
A-esh <abrahamescalona@live.com>
2025-10-14 12:53:55 +0200

    Refactor App class to initialize queues for premium and non-premium subscriptions based on match type

--- 04_colas/App.java
+++ 04_colas/App.java
@@ -1,9 +1,16 @@
 
 import java.util.Queue;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class App {
 
     public static void main(String[] args) {
+        Queue<GameQueue> premiunLargo = new PriorityBlockingQueue<>();
+        Queue<GameQueue> premiunCorto = new PriorityBlockingQueue<>();
+        Queue<GameQueue> noPremiunLargo = new LinkedBlockingQueue<>();
+        Queue<GameQueue> noPremiunCorto = new LinkedBlockingQueue<>();
+
         try {
             FicheroSecuencial<GameQueue> sf =
                     new FicheroSecuencial<GameQueue>("player_requests.csv", ";");
@@ -11,7 +18,20 @@ public class App {
             while (!sf.isEndOfFile()) {
                 GameQueue gameQueue = new GameQueue();
                 sf.read(gameQueue);
+                if(gameQueue.isPremiumSubscription() == true){
+                    if(gameQueue.getMatchType() == 'L'){
+                        premiunLargo.offer(gameQueue);
+                    }else if(gameQueue.getMatchType() == 'S'){
+                        premiunCorto.offer(gameQueue);
+                    }
+                }else if(gameQueue.isPremiumSubscription() == false){
+                    if(gameQueue.getMatchType() == 'L'){
+                        noPremiunLargo.offer(gameQueue);
+                    }else if(gameQueue.getMatchType() == 'S'){
+                        noPremiunCorto.offer(gameQueue);
+                    }
             }
+        }
             sf.close();
         } catch (java.io.FileNotFoundException e) {
             System.err.println("File not found: " + e.getMessage());
