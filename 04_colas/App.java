a9cc692ae212fa97abd77a459d76dbc34444c432
A-esh <abrahamescalona@live.com>
2025-10-16 09:43:53 +0200

    Refactor doMatchs method to accept queues as parameters and enhance match logging

--- 04_colas/App.java
+++ 04_colas/App.java
@@ -11,37 +11,74 @@ public class App {
         Queue<GameRequest> noPremiunLargo = new LinkedBlockingQueue<>();
         Queue<GameRequest> noPremiunCorto = new LinkedBlockingQueue<>();
 
-        readRequests("player_requests.csv", premiunLargo, premiunCorto, noPremiunLargo, noPremiunCorto);
+        readRequests("player_requests.csv", premiunLargo, premiunCorto, noPremiunLargo,
+                noPremiunCorto);
 
-        doMatchs();
+        doMatchs(premiunLargo, premiunCorto, noPremiunLargo, noPremiunCorto);
     }
 
-    public static int doMatchs(){
+    public static int doMatchs(Queue<GameRequest> premiunLargo, Queue<GameRequest> premiunCorto,
+            Queue<GameRequest> noPremiunLargo, Queue<GameRequest> noPremiunCorto) {
         final int premiun = 2;
         final int noPremiun = 1;
-        
-        return 0;
+        GameRequest player1;
+        GameRequest player2;
+        int nMatchs = 0;
+
+        System.out.println("""
+                =====================
+                Making the matchs
+                =====================""");
+
+        while (premiunLargo.size() >= 2 || premiunCorto.size() >= 2 || noPremiunLargo.size() >= 2
+                || noPremiunCorto.size() >= 2) {
+            for (int i = 0; i < premiun && premiunLargo.size() >= 2; i++) {
+                player1 = premiunLargo.poll();
+                player2 = premiunLargo.poll();
+                nMatchs++;
+                System.out.printf("[PREMIUN] Long match between %s (%d) and %s (%d)\n", player1.getPlayerID(), player1.getSkillLevel(), player2.getPlayerID(), player2.getSkillLevel());
+            }
+            for (int i = 0; i < premiun && premiunCorto.size() >= 2; i++) {
+                player1 = premiunCorto.poll();
+                player2 = premiunCorto.poll();
+                nMatchs++;
+                System.out.printf("[PREMIUN] Short match between %s (%d) and %s (%d)\n", player1.getPlayerID(), player1.getSkillLevel(), player2.getPlayerID(), player2.getSkillLevel());
+            }
+            for (int i = 0; i < noPremiun && noPremiunLargo.size() >= 2; i++) {
+                player1 = noPremiunLargo.poll();
+                player2 = noPremiunLargo.poll();
+                nMatchs++;
+                System.out.printf("[NO PREMIUN] Long match between %s (%d) and %s (%d)\n", player1.getPlayerID(), player1.getSkillLevel(), player2.getPlayerID(), player2.getSkillLevel());
+            }
+            for (int i = 0; i < noPremiun && noPremiunCorto.size() >= 2; i++) {
+                player1 = noPremiunCorto.poll();
+                player2 = noPremiunCorto.poll();
+                nMatchs++;
+                System.out.printf("[NO PREMIUN] Short match between %s (%d) and %s (%d)\n", player1.getPlayerID(), player1.getSkillLevel(), player2.getPlayerID(), player2.getSkillLevel());
+            }
+        }
+
+        return nMatchs;
     }
 
     public static int readRequests(String filename, Queue<GameRequest> premiunLargo,
             Queue<GameRequest> premiunCorto, Queue<GameRequest> noPremiunLargo,
             Queue<GameRequest> noPremiunCorto) {
-            int nReaded = 0;
+        int nReaded = 0;
         try {
             FicheroSecuencial<GameRequest> sf =
                     new FicheroSecuencial<GameRequest>("player_requests.csv", ";");
             sf.skip();
             System.out.println("""
-            =====================
-            Reading game requests
-            =====================""");
+                    =====================
+                    Reading game requests
+                    =====================""");
             while (!sf.isEndOfFile()) {
                 nReaded++;
                 GameRequest request = new GameRequest();
                 sf.read(request);
                 System.out.println("New request: " + request);
-                addRequest(request, premiunLargo, premiunCorto, noPremiunLargo,
-                        noPremiunCorto);
+                addRequest(request, premiunLargo, premiunCorto, noPremiunLargo, noPremiunCorto);
             }
             sf.close();
         } catch (java.io.FileNotFoundException e) {
