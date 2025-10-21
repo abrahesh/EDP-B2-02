d6939ce2bc59808cbb174cf2a4b97fd34cd0da54
rarauw2 <raul.arroyo3@alu.uclm.es>
2025-10-21 12:38:14 +0200

    Remove ListRequest class and update Principal class structure

--- 05_listas/Principal.java
+++ 05_listas/Principal.java
@@ -1,3 +1,26 @@
+import java.util.List;
+import java.util.Queue;
+import java.util.ArrayList;
+
 public class Principal {
+
+    private static final int MAIN_START = 14 * 60;
+    private static final int RIVER_START = 13 * 60 + 30;
+    private static final int BREAK_TIME = 30;
+    public static void main(String[] args) {
+        List<Artist> day1_River = new ArrayList<>();
+        List<Artist> day1_Main = new ArrayList<>();
+        List<Artist> day2_River = new ArrayList<>();
+        List<Artist> day2_Main = new ArrayList<>();
+        List<Artist> day3_River = new ArrayList<>();
+        List<Artist> day3_Main = new ArrayList<>();
+
+
+
+        
+    }
+   
+    public static void addList()
+
     
 }
